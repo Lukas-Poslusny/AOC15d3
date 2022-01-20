d5063fbdd86813616512c496d2a136ef8968487d
Lukas-Poslusny <lukas.poslusny@educanet.cz>
2022-01-20 17:05:13 +0100

    done

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Coordinate {
 
     int x;
@@ -7,5 +9,18 @@ public class Coordinate {
     public Coordinate(int cordX, int cordY) {
         x = cordX;
         y = cordY;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y && presents == that.presents;
     }
 }
